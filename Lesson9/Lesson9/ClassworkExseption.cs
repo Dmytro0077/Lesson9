bb765222cd0741024889c6a4f714ac9f45116fe0
Dmytro0077 <kaktusgren@gmail.com>
2022-08-29 18:35:49 +0300

    complete homework

---
+++ Lesson9/Lesson9/ClassworkExseption.cs
@@ -0,0 +1,236 @@
+﻿//namespace ClassworkException
+//{
+//    internal class Classwork
+//    {
+//        static void Main()
+//        {
+//            ////Приклад1
+//            //int x = 5;
+//            //int y = x / 0;
+//            //Console.WriteLine($"Результат: {y}");
+//            //Console.WriteLine("Кінець програми");
+
+
+//            ////Приклад2
+//            //try
+//            //{
+//            //    int x = 5;
+//            //    int y = x / 0;
+//            //    Console.WriteLine($"Результат: {y}");
+//            //}
+//            //catch
+//            //{
+//            //    Console.WriteLine("Виникло виключення!");
+//            //}
+//            //finally
+//            //{
+//            //    Console.WriteLine("Блок finally");
+//            //}
+//            //Console.WriteLine("Кінець програми");
+
+
+//            ////Приклад3
+//            //try
+//            //{
+//            //    int x = 5;
+//            //    int y = x / 0;
+//            //    Console.WriteLine($"Результат: {y}");
+//            //}
+//            //catch
+//            //{
+//            //    Console.WriteLine("Виникло виключення!");
+//            //}
+//            //Console.WriteLine("Кінець програми");
+
+
+//            ////Приклад4
+//            //try
+//            //{
+//            //    int x = 5;
+//            //    int y = x / 0;
+//            //    Console.WriteLine($"Результат: {y}");
+//            //}            
+//            //finally
+//            //{
+//            //    Console.WriteLine("Блок finally");
+//            //}
+//            //Console.WriteLine("Кінець програми");
+
+//            ////Приклад7
+//            //try
+//            //{
+//            //    int x = 5;
+//            //    int y = x / 0;
+//            //    Console.WriteLine($"Результат: {y}");
+//            //}
+//            //catch (DivideByZeroException)
+//            //{
+//            //    Console.WriteLine("Виникло виключення DivideByZeroException!");
+//            //}
+
+
+//            ////Приклад8
+//            //try
+//            //{
+//            //    int x = 5;
+//            //    int y = x / 0;
+//            //    Console.WriteLine($"Результат: {y}");
+//            //}
+//            //catch (DivideByZeroException ex)
+//            //{
+//            //    Console.WriteLine($"Виникло виключення! {ex.Message}");
+//            //}
+
+
+//            ////Приклад9
+//            //int x = 1;
+//            //int y = 0;
+//            //try
+//            //{
+//            //    int result1 = x / y;
+//            //    int result2 = y / x;
+//            //}
+//            //catch (DivideByZeroException) when (y == 0)
+//            //{
+//            //    Console.WriteLine("y не повинен дорівнювати 0");
+//            //}
+//            //catch (DivideByZeroException ex)
+//            //{
+//            //    Console.WriteLine(ex.Message);
+//            //}
+
+
+//            ////Приклад10
+//            //try
+//            //{
+//            //    int x = 5;
+//            //    int y = x / 0;
+//            //    Console.WriteLine($"Результат: {y}");
+//            //}
+//            //catch (Exception ex)
+//            //{
+//            //    Console.WriteLine($"Виключення: {ex.Message}");
+//            //    Console.WriteLine($"Метод: {ex.TargetSite}");
+//            //    Console.WriteLine($"Трасування стеку: {ex.StackTrace}");
+//            //}
+
+
+//            ////Приклад10
+//            //try
+//            //{
+//            //    int[] numbers = new int[4];
+//            //    numbers[7] = 9;     // IndexOutOfRangeException
+//            //    int x = 5;
+//            //    int y = x / 0;  // DivideByZeroException
+//            //    Console.WriteLine($"Результат: {y}");
+//            //}
+//            //catch (DivideByZeroException)
+//            //{
+//            //    Console.WriteLine("Виникло виключення DivideByZeroException");
+//            //}
+//            //catch (IndexOutOfRangeException ex)
+//            //{
+//            //    Console.WriteLine(ex.Message);
+//            //}
+
+//            //Console.Read();
+
+//            ////Приклад11
+//            //catch (Exception ex)
+//            //{
+//            //    Console.WriteLine($"Исключение: {ex.Message}");
+//            //}
+
+
+//            ////Приклад12
+//            //try
+//            //{
+//            //    Console.Write("Введіть ім'я: ");
+//            //    string? name = Console.ReadLine();
+//            //    if (name == null || name.Length < 2)
+//            //    {
+//            //        throw new Exception("Довжина імені менше 2 символів");
+//            //    }
+//            //    else
+//            //    {
+//            //        Console.WriteLine($"Ваше ім'я: {name}");
+//            //    }
+//            //}
+//            //catch (Exception e)
+//            //{
+//            //    Console.WriteLine($"Помилка: {e.Message}");
+//            //}
+
+
+//            ////Приклад13
+//            //Console.Write("Введіть ім'я: ");
+//            //string? name = Console.ReadLine();
+//            //if (name == null || name.Length < 2)
+//            //{
+//            //    throw new Exception("Довжина імені менше 2 символів");
+//            //}
+//            //else
+//            //{
+//            //    Console.WriteLine($"Ваше ім'я: {name}");
+//            //}
+
+
+//            //Приклад14
+//            try
+//            {
+//                try
+//                {
+//                    Console.Write("Введіть ім'я: ");
+//                    string? name = Console.ReadLine();
+//                    if (name == null || name.Length < 2)
+//                    {
+//                        throw new Exception("Довжина імені менше 2 символів");
+//                    }
+//                    else
+//                    {
+//                        Console.WriteLine($"Ваше ім'я: {name}");
+//                    }
+//                }
+//                catch (Exception e)
+//                {
+//                    Console.WriteLine($"Помилка: {e.Message}");
+//                    throw;
+//                }
+//            }
+//            catch (Exception ex)
+//            {
+//                Console.WriteLine(ex.Message);
+//            }
+
+
+
+//            ////Приклад5 та 6 (частина коду нижче)
+//            //Square("12");
+//            //Square("ab");
+
+//        }
+
+//        ////Приклад5
+//        //static void Square(string data)
+//        //{
+//        //    int x = int.Parse(data);
+//        //    Console.WriteLine($"Квадрат числа {x}: {x * x}");
+//        //}
+
+
+//        ////Приклад6
+//        //static void Square(string data)
+//        //{
+//        //    if (int.TryParse(data, out var x))
+//        //    {
+//        //        Console.WriteLine($"Квадрат числа {x}: {x * x}");
+//        //    }
+//        //    else
+//        //    {
+//        //        Console.WriteLine("Некоректний ввід");
+//        //    }
+//        //}
+
+
+//    }
+//}
---
+++ Lesson9/Lesson9/Homework9.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Lesson9
+{
+    class Homework9
+    {
+        static void Main(string[] args)
+        {
+            string[] phoneBook = { "Brad Pitt - 55562578699",
+                                   "Denzel Washington - 5554238946",
+                                   "Sean Connery - 5558723458" };
+
+            int counter;
+            string? answer;
+            Exception exception = null;
+
+            List<string> result = new List<string>();
+
+            do
+            {
+                try
+                {
+                    exception = null;
+                    Console.WriteLine("Введіть літери або цифри для пошуку контакту:");
+                    answer = Console.ReadLine();
+
+                    if (answer == string.Empty)
+                    {
+                        exception = new Exception("Ще раз, введіть літери або цифри для пошуку контакту:");
+                        throw exception;
+                    }
+                    else
+                    {
+                        Regex regex = new Regex(answer, RegexOptions.IgnoreCase);
+
+                        foreach (var contacts in phoneBook)
+                        {
+                            MatchCollection matches = regex.Matches(contacts);
+                            counter = matches.Count;
+                            if (matches.Count > 0)
+                            {
+                                result.Add(contacts);
+                            }
+                        }
+
+                        if (result.Count > 0)
+                        {
+                            Console.WriteLine("Знайдено співпадіння:");
+                            foreach (var person in result)
+                            {
+                                Console.WriteLine(person);
+                            }
+                        }
+                        else
+                        {
+                            Console.WriteLine("Контактів за вашим запитом не знайдено!");
+                        }
+                    }
+                }
+
+                catch (Exception error)
+                {
+                    Console.WriteLine($"{error.Message}");                   
+                }
+            }
+            while (exception != null);
+        }
+    }
+}
